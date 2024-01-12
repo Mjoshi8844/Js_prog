a79d10a44cfe57ed959a933b7f211d1fb1a0c909
Maitri Joshi <maitrijoshi047@gmail.com>
2024-01-12 18:08:34 +0530

    Errorenous Import/Export

---
+++ Node/Export.js
@@ -0,0 +1,8 @@
+function hii(name) {
+    return `Hi ${name}`;
+  }
+  
+  const a = 10;
+
+//   export * from './Export.js';
+export{hii, a};
---
+++ Node/Import.js
@@ -0,0 +1,4 @@
+ import * as exp from './Export.js';
+// const exp = require('./Export');
+
+console.log(exp.hii('Maitri') + '\nThe Value of a: ', exp.a);
