1bf9fd57e5b4d0a305ce3cd0c3edecb9557373ba
Maitri Joshi <maitrijoshi047@gmail.com>
2024-01-12 16:58:33 +0530

    Events practice updated

--- Node/Event.js
+++ Node/Event.js
@@ -1,20 +1,29 @@
-var fs = require("fs");
-var rs = fs.createReadStream("./demo.html");
+// var fs = require("fs");
+// var rs = fs.createReadStream("./demo.html");
 
-rs.on("open", function () {
-  console.log("The file is open");
+// rs.on("open", function () {
+//   console.log("The file is open");
+// });
+
+// rs.on("data", (chunks) => {
+//   console.log("Reading Chunks of Data:\n", chunks.toString());
+// });
+
+// rs.on("end", () => {
+//   console.log("Reached End of File!");
+// });
+
+// rs.on("error", (err) => {
+//   console.log("Error Occured: ", err);
+// });
+
+// console.log(rs); // this is executing first..
+
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+
+eventEmitter.on('sayHi', (name) => {
+    console.log(`Hi ${name}`);
 });
 
-rs.on("data", (chunks) => {
-  console.log("Reading Chunks of Data:\n", chunks.toString());
-});
-
-rs.on("end", () => {
-  console.log("Reached End of File!");
-});
-
-rs.on("error", (err) => {
-  console.log("Error Occured: ", err);
-});
-
-console.log(rs); // this is executing first..
+eventEmitter.emit('sayHi', 'Maitri');
